bb0a6eb15efeba7d1f3f8ff724535cb79f64d3f7
alpha.dvlpr <alpha.dvlpr@gmail.com>
2024-01-22 18:08:42 +0100

    Set header image with javascript

---
+++ scripts.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    setHeaderImage();
+}
+
+function setHeaderImage() {
+    var image = document.getElementById("header_image");
+    image.src = "resources/images/mini_icon.png";
+}

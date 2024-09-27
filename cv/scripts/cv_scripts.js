8e97486902b67913e7085785430a788ff6cdf6a3
alpha.dvlpr <alpha.dvlpr@gmail.com>
2024-09-27 21:55:38 +0200

    added english version of the site

--- cv/scripts/cv_scripts.js
+++ cv/scripts/cv_scripts.js
@@ -1,10 +1,11 @@
+/* Spanish texts */
 function logoAction() {
-    window.location.href = "../../main.html";
+    window.location.href = "../../index.html";
 }
 
 function setAbout() {
     document.getElementsByClassName("main_text")[0].innerHTML = "\
-    Mi nombre es Aarón Granado, tengo 26 años y actualmente resido en Cáceres. \n\n\
+    Mi nombre es Aarón Granado, tengo 26 años y actualmente resido en Asturias. \n\n\
     Estudié el Grado Superior en Desarrollo de Aplicaciones Multiplataforma y fue antes de entregar la propuesta del TFG cuando \
     decidí que mi carrera profesional iba a comenzar con el desarrollo de iOS, arriesgándome totalmente a suspenderlo \
     (spoiler: aprobé con muy buena nota) ya que cualquier cosa diferente e innovadora (cualquier cosa que no estuviera en sus \
@@ -81,13 +82,106 @@ function setJobs() {
 
     document.getElementsByClassName("minsait")[0].innerHTML = "\
     DESARROLLADOR iOS | MINSAIT (INDRA) | JUNIO 2021 -> ABRIL 2022 \n\n\
-    Desarrollos nuevos y mantenimiento de los existentes con Swift, UIKit y Storiboards. \n\n\
+    Desarrollos nuevos y mantenimiento de los existentes con Swift, UIKit y Storyboards. \n\n\
     Git, Jenkins, Merge Requests, Zeplin. \
     ".replaceAll("\n", "</br>");
 
     document.getElementsByClassName("zimaltec")[0].innerHTML = "\
     DESARROLLADOR iOS | ZIMALTEC SOLUCIONES | JUNIO 2020 -> JUNIO 2021 \n\n\
     Desarrollos nuevos y mantenimiento de los existentes con Swift, UIKit y Storyboards. \n\n\
+    Git, Merge Requests, Realm Database, AppStore Connect, TestFlight. \
+    ".replaceAll("\n", "</br>");
+}
+
+/* English texts */
+function logoEnAction() {
+    window.location.href = "../../../en_index.html";
+}
+
+function setEnAbout() {
+    document.getElementsByClassName("main_text")[0].innerHTML = "\
+    My name is Aaron Granado, I am 26 years old and I currently reside in Asturias. \n\n\
+    I studied the Higher Degree in Multiplatform Application Development and it was before submitting the TFG proposal when I \
+    decided that my professional career was going to begin with iOS development, totally risking failing it \
+    (spoiler: I passed with very good marks) since anything different and innovative (anything that was not in their notes from \
+    the beginning of the century) was strongly penalized by my teachers, but I did not care and I bought a rather emaciated but functional 2010 MacBook \
+    Pro and began to learn how both Swift and XCode work with tutorials on \
+    YouTube and the occasional Udemy course. \n\n\
+    Unfortunately, the year I finished the Degree we were in the middle of the first lockdown of 2020, so I could not do \
+    my FCT, which were replaced by a research paper, but that did not stop me, since during those two months I was \
+    sending my resume all over the internet in the hope of I had to wait for a call from somewhere.\n\n\
+    I did a couple of interviews and was lucky enough that a company in Zaragoza gave me the opportunity to start my career as an \
+    iOS developer. At first, due to the pandemic and the lockdowns, I was working remotely, but after spending the \
+    summer of that year I decided to pack my bags and go to Zaragoza to live and work in person, since I couldn't \
+    imagine working from home. \n\n\
+    Throughout my career I have worked for several companies on completely native iOS projects, including \
+    several GPV projects, a project for one of the main telecommunications companies in Spain and Europe to manage \
+    users' telephone lines, and a VoD project for one of the main audiovisual groups in Spain for which \
+    tvOS is also developed. \n\n\
+    Some of these projects have been developed entirely in SwiftUI, a technology that I am still learning about today, even though it is not being used in the current project.\
+    ".replaceAll("\n", "</br>");
+
+    document.getElementsByClassName("more_info_text")[0].innerHTML = "\
+    On this website you can see my work experience in detail and download my updated CV in PDF format, see all my qualifications and contact me for anything. \n\n\
+    Also links to other pages that I have developed. \n\n\
+    I have created the website entirely myself, applying my knowledge in web development but trying to keep it simple. \
+    ".replaceAll("\n", "</br>");
+}
+
+function setEnQualifs() {
+    document.getElementsByClassName("ciberseguridad")[0].innerHTML = "\
+    IN PROGRESS | INTECSSA \n\n\
+    Master in Corporate Cybersecurityand Ethical Hacking \
+    ".replaceAll("\n", "</br>");
+
+    document.getElementsByClassName("dam")[0].innerHTML = "\
+    JUNE 2020 | IES ÁGORA \n\n\
+    Higher Degree in Multiplatform Application Development \
+    ".replaceAll("\n", "</br>");
+
+    document.getElementsByClassName("ingles")[0].innerHTML = "\
+    JUNE 2016 | PLASENCIA OFFICAL LANGUAGE SCHOOL \n\n\
+    English B2 \
+    ".replaceAll("\n", "</br>");
+
+    document.getElementsByClassName("frances")[0].innerHTML = "\
+    JUNE 2015 | PLASENCIA OFFICAL LANGUAGE SCHOOL \n\n\
+    French B1 \
+    ".replaceAll("\n", "</br>");
+
+    document.getElementsByClassName("fundae")[0].innerHTML = "\
+    MAY 2022 | IT FORMACIÓN Y SERVICIOS \n\n\
+    SQLite Development \
+    ".replaceAll("\n", "</br>");
+
+    document.getElementsByClassName("it_institute")[0].innerHTML = "\
+    OCTOBER 2021 | IT INSTITUTE \n\n\
+    Agile Development Methodologies \
+    ".replaceAll("\n", "</br>");
+}
+
+function setEnJobs() {
+    document.getElementsByClassName("knowmad_mood")[0].innerHTML = "\
+    iOS DEVELOPER | KNOWMADMOOD | APRIL 2023 -> SEPTEMBER 2024 \n\n\
+    New developments and maintenance of existing ones with Swift, UIKit and Storyboards. \n\n\
+    Git, GitLab, Merge Requests, Figma, AppStore Connect, TestFlight. \
+    ".replaceAll("\n", "</br>");
+
+    document.getElementsByClassName("telynet")[0].innerHTML = "\
+    iOS DEVELOPER | TELYNET | MAY 2022 -> APRIL 2023 \n\n\
+    Developments from scratch for various clients in SwiftUI. \n\n\
+    Subversion, SQLite. \
+    ".replaceAll("\n", "</br>");
+
+    document.getElementsByClassName("minsait")[0].innerHTML = "\
+    iOS DEVELOPER | MINSAIT (INDRA) | JUNE 2021 -> APRIL 2022 \n\n\
+    New developments and maintenance of existing ones with Swift, UIKit and Storyboards. \n\n\
+    Git, Jenkins, Merge Requests, Zeplin. \
+    ".replaceAll("\n", "</br>");
+
+    document.getElementsByClassName("zimaltec")[0].innerHTML = "\
+    iOS DEVELOPER | ZIMALTEC SOLUCIONES | JUNE 2020 -> JUNE 2021 \n\n\
+    New developments and maintenance of existing ones with Swift, UIKit and Storyboards. \n\n\
     Git, Merge Requests, Realm Database, AppStore Connect, TestFlight. \
     ".replaceAll("\n", "</br>");
 }
